b4f5ce0102a552ed7d83ef1a1e975fe412124cf4
Rached Makni <rached024@gmail.com>
2022-03-11 22:23:06 +0100

    hotfix/ fixing conflict

--- src/main/java/com/esprit/examen/repositories/FormateurRepository.java
+++ src/main/java/com/esprit/examen/repositories/FormateurRepository.java
@@ -1,23 +1,32 @@
 package com.esprit.examen.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
+
 import com.esprit.examen.entities.Formateur;
 import com.esprit.examen.entities.TypeCours;
+
+
 
 
 @Repository
 public interface FormateurRepository extends JpaRepository<Formateur, Long>{
 
-	
-	
- 	//@Query("SELECT count(*) FROM FORMATEUR where TypeCours=:typeCours")
-	//public Long nombreFormateursImpliquesDansUnCours(@Param("typeCours")TypeCours typeCours);
-	
 
-	
+
+
+
+@Query("SELECT count(*) FROM Formateur where TypeCours=:typeCours")
+public Long nombreFormateursImpliquesDansUnCours(@Param("typeCours")TypeCours typeCours);
+
+Formateur findById(long id);
+
+
 
 }
